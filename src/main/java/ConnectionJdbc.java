66c490f285c5691e5850543128c5239951fe07b0
Preenchimento Planilha <bernardinodeivid@gmail.com>
2021-11-06 23:42:47 -0300

    Conexão MySql Workbench

---
+++ src/main/java/ConnectionJdbc.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJdbc {
+    public static void main(String[] args) {
+
+        String url ="jdbc:mysql://localhost:3306/despesasdb?useTimezone=true&serverTimezone=GMT";
+
+        try {
+            Connection conn = DriverManager.getConnection(url,"root","");
+            System.out.println("Exito");
+        } catch (SQLException e) {
+            System.out.println("Falha");
+            e.printStackTrace();
+        }
+    }
+}
